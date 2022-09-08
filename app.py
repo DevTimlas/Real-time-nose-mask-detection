73ce6522cda4901575d3148c523e05d17802f796
Timilehin <timilehinoladejo@yahoo.com>
2022-09-08 13:45:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Face Mask Detection with TF")
 st.header("Face Mask Detection Test with Tensroflow")
 st.text("Upload a face Image to detect maybe the person is wearing a face mask or not.")
 
-uploaded_file = st.file_uploader("Choose a face Image ...", type="jpg")
+uploaded_file = st.file_uploader("Choose a face Image ...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded FaceImage.', use_column_width=True)
